2c0d8c406611028b7816ac7cdde419d17bae7607
MiKing233 <tqh975680379@gmail.com>
2018-09-30 20:22:38 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,15 +1,12 @@
-packagename="com.miking.steam"
-user_permission={
-  [1]	= "ACCESS_COARSE_LOCATION" ;
-  [2]	= "ACCESS_FINE_LOCATION" ;
-  [3]	= "ACCESS_NETWORK_STATE" ;
-  [4]	= "ACCESS_WIFI_STATE" ;
-  [5]	= "INTERNET" ;
-  [6]	= "WRITE_EXTERNAL_STORAGE" ;
-  } ;
-appname="Steam Go"
 template="tool"
-appver="2.1.4 Beta"
-appcode="214"
+user_permission={
+  [1]	= "ACCESS_NETWORK_STATE" ;
+  [2]	= "ACCESS_WIFI_STATE" ;
+  [3]	= "INTERNET" ;
+  } ;
+packagename="com.miking.steam"
+appcode="215"
+appver="2.1.5"
+appname="Steam Go"
 
 --require "import" import"loadlayout" import"loadmenu" import"loadbitmap" 
