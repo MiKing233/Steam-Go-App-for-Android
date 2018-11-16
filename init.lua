8a7085671e422ad67bca1569e4825de7b51fc762
MiKing233 <tqh975680379@gmail.com>
2018-11-17 00:50:53 +0800

    v2.1.8 Beta Update
    v2.1.8 Beta Open Source Code Update

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
-appname="Steam Go"
-appcode="217"
+appver="2.1.8 Beta"
+appcode="218"
+template="tool"
+packagename="com.miking.steam"
 user_permission={
   [1]	= "ACCESS_NETWORK_STATE" ;
   [2]	= "ACCESS_WIFI_STATE" ;
   [3]	= "INTERNET" ;
   } ;
-template="tool"
-packagename="com.miking.steam"
-appver="2.1.7 Beta"
+appname="Steam Go"
