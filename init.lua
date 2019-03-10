82b3873c9bf803d92dc60ee5ba594462cb35f92b
MiKing233 <tqh975680379@gmail.com>
2019-03-10 10:13:57 +0800

    v2.3.5 Update
    v2.3.5 Open Source Code Update

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
+template="tool"
+packagename="com.miking.steam"
+appcode="235"
+appname="Steam Go"
+appver="2.3.5"
 user_permission={
   [1]	= "ACCESS_NETWORK_STATE" ;
   [2]	= "ACCESS_WIFI_STATE" ;
   [3]	= "INTERNET" ;
   } ;
-packagename="com.miking.steam"
-template="tool"
-appcode="234"
-appver="2.3.4 Happy New Year"
-appname="Steam Go"
